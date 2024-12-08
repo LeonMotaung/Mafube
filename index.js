244d1f6bdd9553eade0b03a3604ea2d0c4f5ae82
Leon Motaung <80112085+LeonMotaung@users.noreply.github.com>
2024-12-08 21:43:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ 
     storage,
-    limits: { fileSize: 10 * 1024 * 1024 } // 10MB limit
+    limits: { fileSize: 50* 1024 * 1024 } // 10MB limit
 });
 
 // Middleware
